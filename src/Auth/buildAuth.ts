fd4bfadb07f6397c2f5238ebdfc1be86f0879cb9
Tikiram Ruiz <tikiram@icloud.com>
2025-01-13 23:12:06 -0600

    fix: buildAuth fix

--- src/Auth/buildAuth.ts
+++ src/Auth/buildAuth.ts
@@ -5,10 +5,9 @@ import ErrorHandlerMiddleware from "../HTTPClient/ErrorHandlerMiddleware";
 import HTTPClient from "../HTTPClient/HTTPClient";
 import Auth from "./Auth";
 
-function buildAuth(): Auth {
-  const base = import.meta.env.VITE_AUTH_API_URL;
+function buildAuth(baseUrl: string): Auth {
 
-  const client = new HTTPClient(base, [new ErrorHandlerMiddleware()], {
+  const client = new HTTPClient(baseUrl, [new ErrorHandlerMiddleware()], {
     decoder: new NiceJSONDecoder(true),
     encoder: new NiceJSONEncoder(true),
     credentials: "include",
