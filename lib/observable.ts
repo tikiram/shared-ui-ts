fa1d1e4df14668a9065bacc9785760078728807e
Tikiram Ruiz <tikiram@icloud.com>
2025-01-14 22:06:31 -0600

    feat: useStoreFirstObjectOfType export

--- lib/observable.ts
+++ lib/observable.ts
@@ -1,15 +1,19 @@
-import type ActionResult from "../src/Observable/ActionResult"
-import type ActionStatus from "../src/Observable/ActionStatus"
+import type ActionResult from "../src/Observable/ActionResult";
+import type ActionStatus from "../src/Observable/ActionStatus";
 import type Observable from "../src/Observable/Observable";
-import observeAction from "../src/Observable/observableAction"
-import observableAsyncAction from "../src/Observable/observableAsyncAction"
+import observeAction from "../src/Observable/observableAction";
+import observableAsyncAction from "../src/Observable/observableAsyncAction";
 import ObservableImpl from "../src/Observable/ObservableImpl";
 import ObservableStoreContext from "../src/Observable/ObservableStoreContext";
 import type Observer from "../src/Observable/Observer";
 import type Subscription from "../src/Observable/Subscription";
 import useTrackObservable from "../src/Observable/useTrackObservable";
-import {useStoreObservableOfType, useSmartViewModel, useStoreViewModel} from "../src/Observable/useViewModel";
-
+import {
+  useStoreObservableOfType,
+  useSmartViewModel,
+  useStoreViewModel,
+  useStoreFirstObjectOfType,
+} from "../src/Observable/useViewModel";
 
 export {
   ActionResult,
@@ -25,7 +29,6 @@ export {
   useTrackObservable,
   useSmartViewModel,
   useStoreViewModel,
-  useStoreObservableOfType
-}
-
-
+  useStoreObservableOfType,
+  useStoreFirstObjectOfType,
+};
