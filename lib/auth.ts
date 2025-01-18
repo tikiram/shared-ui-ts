dfc53289a4305a355bb37b2239d5b52706f46e16
Tikiram Ruiz <tikiram@icloud.com>
2025-01-18 14:12:24 -0600

    feat: AuthMiddleware export

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,6 @@
 import buildAuth from "../src/Auth/buildAuth";
 import AuthStatus from "../src/Auth/AuthStatus";
 import Auth from "../src/Auth/Auth";
+import AuthMiddleware from "../src/Auth/AuthMiddleware";
 
-export { Auth, AuthStatus, buildAuth };
+export { Auth, AuthStatus, buildAuth, AuthMiddleware };
